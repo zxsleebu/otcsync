97d225def23d09c0cd4a98a1ef10e3809d9638b0
0xc0666 <raregodz1337@gmail.com>
2021-09-05 22:09:52 +0300

    Fix killaura (anim) indicators && rename indicators type

--- ! OTCSync.js
+++ ! OTCSync.js
@@ -1897,7 +1897,7 @@ GUI.AddSubtab("GUI");
 GUI.AddCheckbox("Watermark", 31).additional("color");
 GUI.AddCheckbox("Indicators", 32).additional("submenu");
 GUI.AddCheckbox("Indicators centered", 45).master("Indicators")//.flags(GUI.SAME_LINE);
-GUI.AddDropdown("Indicators type", ['Default', 'New', 'New v2', 'New v3', 'New v4']).master("Indicators");
+GUI.AddDropdown("Indicators type", ['Default', 'Acidtech', 'Acidtech (inv check)', 'Killaura.lua', 'Killaura.lua (anim)']).master("Indicators");
 GUI.AddCheckbox("Indicators custom color", 61).master("Indicators").additional("color");
 GUI.AddCheckbox("Keybind list", 33).additional("color");
 GUI.AddCheckbox("Spectator list", 34).additional("color");
@@ -3459,15 +3459,15 @@ function indicators(){
 		var indicator = indicators_paths[indicator_path];
 		var active = indicator[2].apply(null, indicator[1]) && !Input.IsKeyPressed(9)/* && !local_buymenu_opened*/;
 		if ((indicator[4] = Clamp(indicator[4] += speed * (active && 1 || -1), 0, 255)) <= 0) continue;
-		var marginy = not_def && type !== 3 && type !== 4 ? 20 : type === 3 ? 15 : 0;
+		var marginy = not_def && type !== 3 && type !== 4 ? 20 : type === 3 || type === 4 ? 15 : 0;
 		var cY = y - margin + Math.floor((indicator[4] / 255) * margin) + 1 + marginy;
 		var color = custom_color || indicator[3];
 		var text = ((typeof indicator[0] === "function") ? indicator[0](x, cY, centered, color, indicator) : indicator[0]);
 		if (not_def && type !== 3 && type !== 4 && ~("ld|legit aa|freestand".split("|")).indexOf(text)) continue;
 		text = (not_def && text == "auto") ? "peek" : text;
 		text = (not_def) ? text.toUpperCase() : text;
-		Render.StringCustom(x, cY + (type === 3 ? 2 : 1), centered, text, [0, 0, 0, Math.floor(indicator[4] / 1.33)], type === 3 ? fonta : font);
-		Render.StringCustom(x, cY + (type === 3 ? 1 : 0), centered, text, [color[0], color[1], color[2], indicator[4]], type === 3 ? fonta : font);
+		Render.StringCustom(x, cY + (type === 3 || type === 4 ? 2 : 1), centered, text, [0, 0, 0, Math.floor(indicator[4] / 1.33)], type === 3 || type === 4 ? fonta : font);
+		Render.StringCustom(x, cY + (type === 3 || type === 4 ? 1 : 0), centered, text, [color[0], color[1], color[2], indicator[4]], type === 3 || type === 4 ? fonta : font);
 		y += (indicator[4] / 255) * margin;
 	}
 }
